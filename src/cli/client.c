1c5bf3cb4c9ebaf06b4825247d66dba7955d05e8
sacha <sachadef31@gmail.com>
2026-04-10 19:47:22 +0200

    remove warnings

--- src/cli/client.c
+++ src/cli/client.c
@@ -43,12 +43,12 @@ int client_loop(client_t *client)
             if (strncmp(client->buffer, NEW_TEAM, strlen(CLIENT_JOINED)) == 0 || strncmp(client->buffer, NEW_TEAM, strlen(CLIENT_LEFT)) == 0)
                 printf("%s", client->buffer);
             //TODO mettre les events
-            if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_TEAM)) == 0)
-                ;
-            if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_CHANNEL)) == 0)
-                ;
-            if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_THREAD)) == 0)
-                ;
+            // if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_TEAM)) == 0)
+            //     ;
+            // if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_CHANNEL)) == 0)
+            //     ;
+            // if (strncmp(client->buffer, NEW_TEAM, strlen(NEW_THREAD)) == 0)
+            //     ;
             memset(client->buffer, '\0', BUFFER_SIZE);
         }
         if (pfds[1].revents & POLLIN) {
